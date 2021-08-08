599fe2270f5794bd4029205cd00593ad8126851e
juckerj <juckerj@student.ethz.ch>
2021-08-09 00:04:03 +0200

    introduce bot-class

--- bot.py
+++ bot.py
@@ -1,93 +1,71 @@
 import argparse
 import logging
-import time
 
-from typing import Dict
-
-from telegram import ReplyKeyboardMarkup, Update, ReplyKeyboardRemove
+from telegram import ReplyKeyboardMarkup, Update
 from telegram.ext import (
     Updater,
     CommandHandler,
-    MessageHandler,
-    Filters,
     ConversationHandler,
     PicklePersistence,
     CallbackContext,
 )
 
-# Enable logging
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
+class WildBot:
 
-logger = logging.getLogger(__name__)
+    def __init__(self,token):
 
-CHOOSING, TYPING_REPLY, TYPING_CHOICE = range(3)
+        # Create the Updater and pass it your bot's token.
+        persistence = PicklePersistence(filename='backup/bot.pkl')
+        self.updater = Updater(token, persistence=persistence)
 
+        self.dp = self.updater.dispatcher
+        self.dp.add_handler(CommandHandler('start',self._start))
+        self.dp.add_handler(CommandHandler('subscribe',self._subscribe))
+        self.dp.add_handler(CommandHandler('unsubscribe',self._unsubscribe))
 
+        # start the bot
+        self.updater.start_polling()
 
+    def stop(self):
+        self.updater.stop()
 
-def start(update: Update, context: CallbackContext):
-    reply_text = "Hi! I am LonelyCam. If you want to subscribe write /subscribe. \
-                  Write /unsubscribe to no longer receive cute pics"
+    def _start(update: Update, context: CallbackContext):
+        reply_text = "Hi! I am LonelyCam. If you want to subscribe write /subscribe. \
+                      Write /unsubscribe to no longer receive cute pics"
 
-    reply_keyboard = [
-        ['/subscribe', '/unsubscribe'],
-    ]
+        reply_keyboard = [
+            ['/subscribe', '/unsubscribe'],
+        ]
 
-    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
 
-    update.message.reply_text(reply_text, reply_markup=markup)
+        update.message.reply_text(reply_text, reply_markup=markup)
 
-def subscribe(update: Update, context: CallbackContext):
-    reply_text = "You sucessfully subscribed."
-    update.message.reply_text(reply_text)
+    def _subscribe(self,update: Update, context: CallbackContext):
+        reply_text = "You sucessfully subscribed."
+        update.message.reply_text(reply_text)
 
-    # add user to subscription list
-    user_id = update.effective_user.id
-    context.bot_data.setdefault('user_id', set()) # create key if not present
-    context.bot_data['user_id'].add(user_id)
+        # add user to subscription list
+        user_id = update.effective_user.id
+        context.bot_data.setdefault('user_id', set()) # create key if not present
+        context.bot_data['user_id'].add(user_id)
 
-def unsubscribe(update: Update, context: CallbackContext):
-    reply_text = "You sucessfully unsubscribed."
-    update.message.reply_text(reply_text)
+        logging.info(context.bot_data.setdefault('user_id', set()))
 
-    # remove user from subscription list
-    user_id = update.effective_user.id
-    context.bot_data.setdefault('user_id', set()) # create key if not present
-    if user_id in context.bot_data['user_id']:
-        context.bot_data['user_id'].remove(user_id)
-    print(context.bot_data.setdefault('user_id', set()))
+    def _unsubscribe(self,update: Update, context: CallbackContext):
+        reply_text = "You sucessfully unsubscribed."
+        update.message.reply_text(reply_text)
 
+        # remove user from subscription list
+        user_id = update.effective_user.id
+        context.bot_data.setdefault('user_id', set()) # create key if not present
+        if user_id in context.bot_data['user_id']:
+            context.bot_data['user_id'].remove(user_id)
 
-def main() -> None:
-    """Run the bot."""
+        logging.info(context.bot_data.setdefault('user_id', set()))
 
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('--bot_token', \
-                        dest='bot_token', \
-                        type=str, \
-                        help='unique token of bot (KEEP PRIVATE!)')
-
-
-    args = parser.parse_args()
-
-    # Create the Updater and pass it your bot's token.
-    persistence = PicklePersistence(filename='backup/bot.pkl')
-    updater = Updater(args.bot_token, persistence=persistence)
-
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler('start',start))
-    dp.add_handler(CommandHandler('subscribe',subscribe, pass_args=True))
-    dp.add_handler(CommandHandler('unsubscribe',unsubscribe, pass_args=True))
-
-    # start the bot
-    updater.start_polling()
-    time.sleep(10)
-    updater.stop()
-
-    #updater.idle()
-
-if __name__ == '__main__':
-    main()
+    def broadcast(self):
+        message = 'Hello from subscription'
+        for user_id in self.dp.bot_data['user_id']:
+            logging.info(user_id)
+            self.dp.bot.send_message(chat_id=user_id, text=message)
--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 import RPi.GPIO as GPIO
+import logging
 
 from sensors import Pir
 from cam import WildCam
+from bot import WildBot
 
 def main():
+
+    # Enable logging
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+    )
+
+    logger = logging.getLogger(__name__)
+
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--bot_token', \
+                        dest='bot_token', \
+                        type=str, \
+                        help='unique token of bot (KEEP PRIVATE!)')
+
+    args = parser.parse_args()
 
     GPIO.setmode(GPIO.BCM)
 
@@ -11,20 +30,20 @@ def main():
     pir.activate()
 
     cam = WildCam()
+    bot = WildBot(args.bot_token)
 
     try:
         while True:
             pir.wait_for_movement()
             cam.shot(nr_of_shots=3,pause=1,night_mode=True)
             cam.record(3,night_mode=True)
+            bot.broadcast()
 
     except KeyboardInterrupt:
         cam.close()
+        bot.stop()
         pir.deactivate()
-        print('Cleanup')
-
-
-
+        logging.info('Cleanup')
 
 if __name__ == '__main__':
     main()
